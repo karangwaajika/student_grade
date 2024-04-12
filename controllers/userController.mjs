d04d98ae42cdc6dc5289be5144d8444a82e43abc
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2024-04-12 10:38:31 +0200

    add user using form

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -3,14 +3,21 @@ import {validationResult, matchedData} from 'express-validator'
 import {hashPassword} from '../utils/hashingPassword.mjs'
 
 export const addUser = async(request, response)=>{
-    const result = validationResult(request)
-    if(result.errors.length != 0){
-        response.status(401).send({"errors":result.errors})
-    }else{
-        const data = matchedData(request)
-        console.log(data)
-        data.password = hashPassword(data.password)
-        const user = new User(data)
+    // console.log(request.body)
+    // console.log(request.file.filename) 
+    // const result = validationResult(request)
+    // if(result.errors.length != 0){
+    //     response.status(401).send({"errors":result.errors})
+    // }else{
+        //const data = matchedData(request)
+        const {body, file} = request
+        console.log(body.email, file.filename)
+        body.password = hashPassword(body.password)
+        const user = new User({
+            email: body.email,
+            password: body.password,
+            image: file.filename
+        })
         try{
             const savedUser = await user.save()
             response.status(200).send(savedUser)
@@ -18,5 +25,5 @@ export const addUser = async(request, response)=>{
         catch(err){ 
             response.status(400).send({'error': err})
         }
-    }
+    //}
 }
--- route/userRoute.mjs
+++ route/userRoute.mjs
@@ -2,8 +2,19 @@ import express from 'express'
 import * as controller from '../controllers/userController.mjs'
 import { checkSchema } from 'express-validator'
 import { userValidation } from '../utils/userValidation.mjs'
+import multer from 'multer'
 const route = express()
 
-route.post('/add_user', checkSchema(userValidation), controller.addUser)
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+      cb(null, './public/img')
+    },
+    filename: (req, file, cb)=>{
+      cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname)
+    }
+  }) 
+const upload = multer({ storage: storage })
 
-export default route
+route.post('/add_user', upload.single('image') ,controller.addUser)
+
+export default route 
